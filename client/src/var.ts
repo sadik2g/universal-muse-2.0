a5a2fcf378fc3f5dcf8fbfc434554061c562d8f1
Abubakar Ibrahim <sadeeq3g@gmail.com>
2025-12-10 17:08:24 +0100

    Revert to local uploads and normal routes

--- client/src/var.ts
+++ client/src/var.ts
@@ -1,8 +1,2 @@
 export const API_URL = "https://www.universalmuse.live/api";
-
-export const ASSETS_URL = "http://localhost:3000"; // Make sure it does not end with a slash
-
-
-
-
-
+export const ASSETS_URL = "https://www.universalmuse.live";
