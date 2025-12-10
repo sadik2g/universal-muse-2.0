ee1c91bedd0ddbfb281978e8a65715d07a5bb196
Abubakar Ibrahim <sadeeq3g@gmail.com>
2025-12-10 17:31:42 +0100

    Remove PayPal, clean main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-createRoot(document.getElementById("root")!).render(
-  <PayPalScriptProvider
-    options={{
-      "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID || "",
-      currency: "USD",
-    }}
-  >
-    <App />
-  </PayPalScriptProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
